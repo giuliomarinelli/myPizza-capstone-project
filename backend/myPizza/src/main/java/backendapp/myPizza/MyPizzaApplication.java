5b28573b852155f48af4dc520d28b7b8e3f58f4d
giuliomarinelli <giulio.marinelli@icloud.com>
2024-03-03 15:57:36 +0100

    init develop

---
+++ backend/myPizza/src/main/java/backendapp/myPizza/MyPizzaApplication.java
@@ -0,0 +1,13 @@
+package backendapp.myPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPizzaApplication.class, args);
+	}
+
+}
---
+++ backend/myPizza/src/test/java/backendapp/myPizza/MyPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package backendapp.myPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

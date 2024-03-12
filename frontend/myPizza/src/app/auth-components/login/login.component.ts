395133647183fe4df58fab1a556dc59e1dd54209
giuliomarinelli <giulio.marinelli@icloud.com>
2024-03-12 13:24:50 +0100

    goin on with backend

--- frontend/myPizza/src/app/auth-components/login/login.component.ts
+++ frontend/myPizza/src/app/auth-components/login/login.component.ts
@@ -24,62 +24,62 @@ export class LoginComponent {
 
 
 
-ngDoCheck() {
-  this.errorMsg = `${this.setInvalidMessages('email')}${this.setInvalidMessages('password')}`
-  this.valid = this.loginForm['valid']
-}
+  ngDoCheck() {
+    this.errorMsg = `${this.setInvalidMessages('email')}${this.setInvalidMessages('password')}`
+    this.valid = this.loginForm['valid']
+  }
 
   protected loginForm: FormGroup = this.fb.group({
-  email: [null, [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
-  password: [null, [Validators.required]]
-})
+    email: [null, [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
+    password: [null, [Validators.required]]
+  })
 
   private setInvalidMessages(fieldName: string): string {
-  const field: AbstractControl | null = this.loginForm.get(fieldName)
-  let errorMsg = ''
-  if (field) {
-    if (field.errors) {
-      if (field.errors['required'] && fieldName === 'email' && field.dirty) errorMsg += 'Email obbligatoria. '
-      if (field.errors['required'] && fieldName === 'password' && field.dirty) errorMsg += 'Password obbligatoria. '
-      if (field.errors['pattern']) errorMsg += 'Formato email errato. '
-    }
+    const field: AbstractControl | null = this.loginForm.get(fieldName)
+    let errorMsg = ''
+    if (field) {
+      if (field.errors) {
+        if (field.errors['required'] && fieldName === 'email' && field.dirty) errorMsg += 'Email obbligatoria. '
+        if (field.errors['required'] && fieldName === 'password' && field.dirty) errorMsg += 'Password obbligatoria. '
+        if (field.errors['pattern']) errorMsg += 'Formato email errato. '
+      }
 
+    }
+    return errorMsg
   }
-  return errorMsg
-}
 
   protected isValid(fieldName: string) {
-  return this.loginForm.get(fieldName)?.valid && this.loginForm.get(fieldName)?.dirty
-}
+    return this.loginForm.get(fieldName)?.valid && this.loginForm.get(fieldName)?.dirty
+  }
 
   protected isInvalid(fieldName: string) {
-  return !this.loginForm.get(fieldName)?.valid && this.loginForm.get(fieldName)?.dirty
-}
+    return !this.loginForm.get(fieldName)?.valid && this.loginForm.get(fieldName)?.dirty
+  }
 
   protected performLogin(): void {
-  if(this.loginForm.valid) {
-  this.authSvc.login(this.loginForm.value).subscribe(
+    if (this.loginForm.valid) {
+      this.authSvc.login(this.loginForm.value).subscribe(
 
-    res => {
-      this.authSvc.loggedInSbj.next(true)
-      console.log(this.currentRoute)
-      if (this.currentRoute === '/login') this.router.navigate(['/my-pizza'])
-    },
-    err => {
-      this.validLogin = false
-      console.log(err.error.message)
-      if (err.error.message === 'Email and/or password are incorrect') this.loginErrorMsg = 'Email o password errate'
-      console.log(this.errorMsg)
+        res => {
+          this.authSvc.loggedInSbj.next(true)
+          console.log(this.currentRoute)
+          if (this.currentRoute === '/login') this.router.navigate(['/my-pizza'])
+        },
+        err => {
+          this.validLogin = false
+          console.log(err.error.message)
+          if (err.error.message === 'Email and/or password are incorrect') this.loginErrorMsg = 'Email o password errate'
+          console.log(this.errorMsg)
+        }
+      )
+    } else {
+      Object.values(this.loginForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
     }
-  )
-} else {
-  Object.values(this.loginForm.controls).forEach(control => {
-    if (control.invalid) {
-      control.markAsDirty();
-      control.updateValueAndValidity({ onlySelf: true });
-    }
-  });
-}
   }
 
 

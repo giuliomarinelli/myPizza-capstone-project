15f5cac5d5b031d5c72ab510fb31a33b70cbaffc
giuliomarinelli <giulio.marinelli@icloud.com>
2024-04-04 17:25:20 +0200

    nest

--- backend_nodejs_nest_fastify/my-pizza/src/app.module.ts
+++ backend_nodejs_nest_fastify/my-pizza/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configurations } from './config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    load: [...configurations],
+    isGlobal: true,
+    envFilePath: ['development.env']
+  }),
+   TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configSvc: ConfigService) => configSvc.get('DB'),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- backend_nodejs_nest_fastify/my-pizza/src/main.ts
+++ backend_nodejs_nest_fastify/my-pizza/src/main.ts
@@ -1,11 +1,13 @@
-/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import fastifyCookie from '@fastify/cookie';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule,
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
     new FastifyAdapter(),
     {
       cors: {
@@ -13,11 +15,17 @@ async function bootstrap() {
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
       }
-    });
-
+    })
   await app.register(fastifyCookie, {
     secret: 'my-secret', // ... variabili d'ambiente
-  });
-  await app.listen(3000);
+  })
+
+  const logger = new Logger('Bootstrap')
+  logger.log('MyPizza Backend Node.js Application')
+
+  const configSvc =  app.get<ConfigService>(ConfigService)
+  
+  await app.listen(configSvc.get('APP.port'));
+
 }
 bootstrap();

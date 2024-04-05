c13d917b680bf53fceb3904e0513b4e2d6ac8308
giuliomarinelli <giulio.marinelli@icloud.com>
2024-04-05 07:23:46 +0200

    added AddressModule, AuthUserModule, ProductModule, authentication, global guard, refresh auth, cookies

--- backend_nodejs_nest_fastify/my-pizza/src/app.module.ts
+++ backend_nodejs_nest_fastify/my-pizza/src/app.module.ts
@@ -2,6 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configurations } from './config/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressModule } from './nest_modules/address/address.module';
+import { AuthUserModule } from './nest_modules/auth-user/auth-user.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './nest_modules/auth-user/guards/auth.guard';
+import { JwtUtilsService } from './nest_modules/auth-user/services/jwt-utils.service';
+import { JwtService } from '@nestjs/jwt';
+import { ProductModule } from './nest_modules/product/product.module';
+
+
 
 
 
@@ -16,8 +25,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useFactory: (configSvc: ConfigService) => configSvc.get('DB'),
       inject: [ConfigService],
     }),
+   AddressModule,
+   AuthUserModule,
+   ProductModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: AuthGuard
+  },
+  JwtService,
+  ConfigService,
+  JwtUtilsService
+],
 })
 export class AppModule { }

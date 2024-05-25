7dd7be75cd3c635d0e0083d0daf284b2c2a37c3d
giuliomarinelli <giulio.marinelli@icloud.com>
2024-05-25 18:14:52 +0200

    appconfig java

--- backend_java_spring_boot/myPizza/src/main/java/backendapp/myPizza/config/AppConfig.java
+++ backend_java_spring_boot/myPizza/src/main/java/backendapp/myPizza/config/AppConfig.java
@@ -29,38 +29,38 @@ public class AppConfig {
     public PasswordEncoder encoder(){
         return new BCryptPasswordEncoder();
     }
-    @Bean
-    public Cloudinary getCloudinary(@Value("${cloudinary.name}") String name,
-                                    @Value("${cloudinary.api_key}") String key,
-                                    @Value("${cloudinary.api_secret}") String secret){
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", name,
-                "api_key",key,
-                "api_secret", secret));
-    }
-    @Bean
-    public JavaMailSenderImpl getMailSender(
-            @Value("${mail.smtp.host}") String smtpHost,
-            @Value("${mail.smtp.port}") String port,
-            @Value("${mail.from}") String from,
-            @Value("${mail.password}") String password,
-            @Value("${mail.transport.protocol}") String protocol,
-            @Value("${mail.smtp.auth}") String auth,
-            @Value("${mail.smtp.starttls.enable}") String starttls,
-            @Value("${mail.debug}") String debug,
-            @Value("${mail.ssl.enable}") String sslEnable
-    ) {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(smtpHost);
-        mailSender.setPort(Integer.parseInt(port));
-        mailSender.setUsername(from);
-        mailSender.setPassword(password);
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", protocol);
-        props.put("mail.smtp.auth", auth);
-        props.put("mail.smtp.starttls.enable", starttls);
-        props.put("mail.debug", debug);
-        props.put("smtp.ssl.enable", sslEnable);
-        return mailSender;
-    }
+//    @Bean
+//    public Cloudinary getCloudinary(@Value("${cloudinary.name}") String name,
+//                                    @Value("${cloudinary.api_key}") String key,
+//                                    @Value("${cloudinary.api_secret}") String secret){
+//        return new Cloudinary(ObjectUtils.asMap(
+//                "cloud_name", name,
+//                "api_key",key,
+//                "api_secret", secret));
+//    }
+//    @Bean
+//    public JavaMailSenderImpl getMailSender(
+//            @Value("${mail.smtp.host}") String smtpHost,
+//            @Value("${mail.smtp.port}") String port,
+//            @Value("${mail.from}") String from,
+//            @Value("${mail.password}") String password,
+//            @Value("${mail.transport.protocol}") String protocol,
+//            @Value("${mail.smtp.auth}") String auth,
+//            @Value("${mail.smtp.starttls.enable}") String starttls,
+//            @Value("${mail.debug}") String debug,
+//            @Value("${mail.ssl.enable}") String sslEnable
+//    ) {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost(smtpHost);
+//        mailSender.setPort(Integer.parseInt(port));
+//        mailSender.setUsername(from);
+//        mailSender.setPassword(password);
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", protocol);
+//        props.put("mail.smtp.auth", auth);
+//        props.put("mail.smtp.starttls.enable", starttls);
+//        props.put("mail.debug", debug);
+//        props.put("smtp.ssl.enable", sslEnable);
+//        return mailSender;
+//    }
 }
